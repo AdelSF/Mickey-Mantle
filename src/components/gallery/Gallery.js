205d0db130b281a668ddc26be949ddc110efde34
Adel Pakrad <adelpakrad@gmail.com>
2021-08-19 01:27:00 -0700

    adding cards into assets for gallery

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 export default function Gallery() {
     return (
         <Container>
+            <TopImgMicky src="../src/assets/cards/1952.gif" alt="Micky Hits The Ball" />
+
             Gallery component
         </Container>
     )
@@ -12,3 +14,9 @@ export default function Gallery() {
 const Container = styled.div`
     color: red;
 `
+const TopImgMicky = styled.img`
+    width: 20%;
+    margin: 0 50px;
+    /* @media (max-width: 1200px) {
+    } */
+    `
